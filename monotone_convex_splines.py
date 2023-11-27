ed5b9fb1293f5544378c8bb2c30b3d1015e99efa
Fatih Çatalkaya <fatih.catalkaya@yahoo.de>
2023-11-27 22:42:10 +0100

    Continue on monotone convex interpolation

--- monotone_convex_splines.py
+++ monotone_convex_splines.py
@@ -1,7 +1,46 @@
+import bisect
+import math
 from datetime import datetime
+from typing import Callable, Any
 
 import numpy as np
 import pandas as pd
+
+
+class InterpolantPiece:
+    def __init__(self, min_x: float, function: Callable[[float], float]):
+        self.min_x = min_x
+        self.function = function
+
+    def __call__(self, *args, **kwargs):
+        return self.function(*args, **kwargs)
+
+
+class Interpolant:
+    def __init__(self):
+        self.pieces: list[InterpolantPiece] = []
+
+    def add_piece(self, interpolant_piece: InterpolantPiece):
+        self.pieces.append(interpolant_piece)
+        self.pieces = sorted(self.pieces, key=lambda piece: piece.min_x)
+
+    def __call__(self, x: float):
+        # Find correct piece
+        piece_pos = bisect.bisect_right(self.pieces, x)
+        piece_pos = min(0, piece_pos - 1)  # bounds check
+        return self.pieces[piece_pos](x)
+
+
+def check_region_1(g0: float, g1: float) -> bool:
+    return (g0 < 0 and -0.5 * g0 <= g1 and g1 <= -2.0 * g0) or (g0 < 0 and -0.5 * g0 >= g1 and g1 >= -2.0 * g0)
+
+
+def check_region_2(g0: float, g1: float) -> bool:
+    return (g0 < 0 and g1 > -2.0 * g0) or (g0 > 0 and g1 < -2.0 * g0)
+
+
+def check_region_3(g0: float, g1: float) -> bool:
+    return (g0 > 0 and 0 > g1 and g1 > -0.5 * g0) or (g0 < 0 and 0 < g1 and g1 < -0.5 * g0)
 
 
 def compute_discrete_forwards(yields: list[float], ytms: list[float]) -> list[float]:
@@ -22,6 +61,40 @@ def compute_instantaneous_forwards(discrete_forwards: list[float]) -> list[float
     return inst_forwards
 
 
+def enforce_monotonicity(ytms: list[float], disc_forwards: list[float], inst_forwards: list[float]):
+    function_g = Interpolant()
+    for i in range(len(disc_forwards)):
+        disc = disc_forwards[i]
+        g0 = inst_forwards[i] - disc
+        g1 = inst_forwards[i + 1] - disc
+        if check_region_1(g0, g1):
+            # Case 1
+            g = lambda x: g0 * (1.0 - 4.0 * x + 3.0 * (x ** 2.0)) + g1 * (-2.0 * x + 3.0 * (x ** 2.0))
+            interpolant_piece = InterpolantPiece(ytms[i], g)
+            function_g.add_piece(interpolant_piece)
+        elif check_region_2(g0, g1):
+            # Case 2
+            eta = (g1 + 2.0 * g0) / (g1 - g0)
+            ytm_first_part = ytms[i] + eta
+            ytm_second_part = ytms[i + 1]
+            g_first_part = lambda x: g0
+            g_second_part = lambda x: g0 + (g1 - g0) * math.pow((x - eta) / (1.0 - eta), 2.0)
+            function_g.add_piece(InterpolantPiece(ytm_first_part, g_first_part))
+            function_g.add_piece(InterpolantPiece(ytm_second_part, g_second_part))
+        elif check_region_3(g0, g1):
+            # Case 3
+            eta = 3.0*(g1/(g1-g0))
+            ytm_first_part = ytms[i] + eta
+            ytm_second_part = ytms[i + 1]
+            g_first_part = lambda x: g1+(g0-g1)*math.pow((eta-x)/eta, 2)
+            g_second_part = lambda x: g1
+            function_g.add_piece(InterpolantPiece(ytm_first_part, g_first_part))
+            function_g.add_piece(InterpolantPiece(ytm_second_part, g_second_part))
+        else:
+            # Case 4
+            continue
+
+
 def do_stuff(df: pd.DataFrame, today: datetime):
     # unique_maturities = df["maturity"].unique()
     # yields = []
@@ -35,4 +108,5 @@ def do_stuff(df: pd.DataFrame, today: datetime):
 
     discrete_forwards = compute_discrete_forwards(yields, ytms)
     instantaneous_forwards = compute_instantaneous_forwards(discrete_forwards)
+    enforce_monotonicity(discrete_forwards, instantaneous_forwards)
     print(instantaneous_forwards)
